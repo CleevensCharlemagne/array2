58b966f9f02498099ebe88988077ba054b21f60f
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-11 21:50:36 -0500

    update

--- main.c
+++ main.c
@@ -5,6 +5,10 @@ int main () {
    int n[ 10 ]; /* n is an array of 10 integers */
    int i,j;
 
+   /* initialize elements of array n to 0 */
+   for ( i = 0; i < 10; i++ ) {
+      n[ i ] = i + 100; /* set element at location i to i + 100 */
+   }
 
 
    return 0;
